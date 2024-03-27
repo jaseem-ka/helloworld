63d2002a2ec1b244cc1b9f9d9f6512e844ef9c39
jaseem-ka <jaseem727k@gmail.com>
2024-03-27 11:07:38 +0530

    first commit

---
+++ app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+     function hello()
+    {
+        return "Hello World";
+    }
+}
+
